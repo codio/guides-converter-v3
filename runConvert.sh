bde1b8c2ae18f602a644df2c4537abb4c0f61111
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-08-19 11:29:37 +0300

    +x

--- runConvert.sh
+++ runConvert.sh
@@ -2,6 +2,7 @@
 
 CONVERTER_VERSION="72ecc41ef93e390bf6d9287eb13b9809e4f82c60"
 
+
 curl "https://static-assets.codio.com/guides-converter-v3/guides-converter-v3-${CONVERTER_VERSION}" --output guides-converter-v3
 chmod +x ./guides-converter-v3
 ./guides-converter-v3
