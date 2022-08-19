5745feb77156b1950eaba159e6177bd2e169fcf5
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-08-19 15:38:19 +0300

    cleanup

--- runConvert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONVERTER_VERSION="72ecc41ef93e390bf6d9287eb13b9809e4f82c60"
-
-
-curl "https://static-assets.codio.com/guides-converter-v3/guides-converter-v3-${CONVERTER_VERSION}" --output guides-converter-v3
-chmod +x ./guides-converter-v3
-./guides-converter-v3
-rm guides-converter-v3
