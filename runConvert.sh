52f8c5abe1be7a9cc1703ac954f02bead969a615
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-08-22 11:11:54 +0300

    typo

--- runConvert.sh
+++ runConvert.sh
@@ -3,7 +3,7 @@
 converterVersion=$1
 
 
-curl "https://static-assets.codio.com/guides-converter-v3/guides-converter-v3-${converterVersion}" --output guides-converter-v
+curl "https://static-assets.codio.com/guides-converter-v3/guides-converter-v3-${converterVersion}" --output guides-converter-v3
 chmod +x ./guides-converter-v3
 ./guides-converter-v3
 rm guides-converter-v3
