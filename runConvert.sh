d4aa51a80e5dbf0045b2ab31e1f81e54b7f5b755
sergei-bronnikov <sergeibronnikov87@gmail.com>
2022-08-22 12:02:48 +0300

    exit code

--- runConvert.sh
+++ runConvert.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 converterVersion=$1
+exit_code=0
 
+
+check_exit_code () {
+  code=$1
+  if [ $code -eq 1 ]
+  then
+    exit_code=1
+  fi
+}
 
 curl "https://static-assets.codio.com/guides-converter-v3/guides-converter-v3-${converterVersion}" --output guides-converter-v3
-chmod +x ./guides-converter-v3
-./guides-converter-v3
-rm guides-converter-v3
+check_exit_code $?
 
-exit $?
+chmod +x ./guides-converter-v3
+check_exit_code $?
+
+./guides-converter-v3
+check_exit_code $?
+
+rm guides-converter-v3
+check_exit_code $?
+exit $exit_code
